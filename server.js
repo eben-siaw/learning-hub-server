ce21aabd8bc69e39d806d79b018d7b939a333954
eben-siaw <61696999+eben-siaw@users.noreply.github.com>
2020-09-09 18:58:53 +0100

    new commit message

--- server.js
+++ server.js
@@ -12,6 +12,12 @@ app.use(bodyParser.json())
 
 app.use(bodyParser.urlencoded({extended: true}))
 
+app.use(function(req, res, next) { 
+  res.header("Access-Control-Allow-Origin", "*"); 
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 //connection 
 mongoose.connect(process.env.mongoURI, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true })
 .then(() => console.log('MongoDB is Connected..'))
