4aa2a9faa2436a555c752f1c8f78e65b693725a1
eben-siaw <61696999+eben-siaw@users.noreply.github.com>
2020-09-10 13:02:56 +0100

    new commit message

--- server.js
+++ server.js
@@ -15,8 +15,8 @@ app.use(express.json());
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended: true}))
 
-app.use(function(req, res, next) { 
-  res.header("Access-Control-Allow-Origin", "http://localhost:3000"); 
+app.all(function(req, res, next) { 
+  res.header("Access-Control-Allow-Origin", "*"); 
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept"); 
   res.header("Access-Control-Allow-Methods", "POST", "GET", "DELETE", "PUT")
   next();
